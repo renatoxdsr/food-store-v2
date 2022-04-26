36ea6436c83d018642474ed85269ee4009eff68b
Renato Xavier <59875607+renatoxdsr@users.noreply.github.com>
2022-04-26 20:11:04 -0300

    Update Order.java

--- src/main/java/br/edu/ifpb/foodstore/domain/Order.java
+++ src/main/java/br/edu/ifpb/foodstore/domain/Order.java
@@ -1,31 +1,67 @@
-package br.edu.ifpb.foodstore.service.payment;
+package br.edu.ifpb.foodstore.domain;
 
-import br.edu.ifpb.foodstore.service.log.LogHandler;
-import br.edu.ifpb.foodstore.service.log.LogService;
-import br.edu.ifpb.foodstore.service.payment.strategy.Payment;
-import br.edu.ifpb.foodstore.service.payment.strategy.PaymentFactory;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import lombok.*;
+import br.edu.ifpb.foodstore.domain.state.OrderState;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class PaymentService {
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.List;
 
-    public Payment        payment;
-    public PaymentFactory typePayment = new PaymentFactory();
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
 
-    public enum PaymentType {
-        CREDIT_CARD, DEBIT, BILLET, PAYPAL
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar creationDate;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items;
+
+     private Order.OrderEnum status = Order.OrderEnum.IN_PROGRESS;
+
+    public enum OrderEnum implements OrderState {
+        IN_PROGRESS {
+            @Override
+            public String cancel() {
+                return "Canceling in progress order";
+            }
+        }
+        ,CANCELED {
+            @Override
+            public String cancel() {
+                return "Order already canceled!";
+            }
+        }
+        ,PAYMENT_SUCCESS {
+            @Override
+            public String cancel() {
+                return "Canceling already paid order";
+            }
+        }
+        ,PAYMENT_REFUSED {
+            @Override
+            public String cancel() {
+                return "Canceling refused order";
+            }
+        }
+
+    public void addItem(OrderItem item) {
+        this.items.add(item);
     }
 
-    private final LogService logService;
-
-    public void doPayment(PaymentService.PaymentType paymentType) throws Exception {
-        this.payment = this.typePayment.typePayment(paymentType);
-        logService.info(this.payment.getPayment());
+    public void removeItem(OrderItem item) {
+        this.items.remove(item);
     }
 
 }
